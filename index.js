fa164a979efe129066645e183edba336722496c8
joydharsamrat <joydharsamrat@gmail.com>
2022-10-27 02:20:17 +0600

    categories data creacted

--- index.js
+++ index.js
@@ -5,8 +5,21 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const categories = require('./data/categories.json');
+const details = require('./data/details.json');
+
 app.get('/', (req, res) => {
     res.send('happy learning server is running')
+})
+
+app.get('/categories', (req, res) => {
+    res.send(categories)
+})
+
+app.get('/details/:id', (req, res) => {
+    const id = req.params.id;
+    const subjectDetails = details.find(subject => subject.id == id)
+    res.send(subjectDetails)
 })
 
 app.listen(port, () => {
